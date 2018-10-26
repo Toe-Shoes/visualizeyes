d32b7737192d93c737918d8549ad61e25c5257e0
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-10-25 21:32:11 -0400

    webpack config finished

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'webpack-bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['react', 'babel-preset-env', 'es2015']
+            },
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'styler-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, '.'),
+    port: 3000,
+    open: true,
+    hot: true,
+    proxy: {
+      '/app': 'https://localhost:8080'
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './index.html'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
