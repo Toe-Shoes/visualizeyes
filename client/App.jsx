9108aeb4982fa66ce1469424585ec2e24abd0829
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-10-26 15:57:14 -0400

    creating file container where our database will be displayed as folder

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FileContainer from './containers/FileContainer.jsx';
 
 class App extends Component {
   constructor(props) {
@@ -7,8 +8,8 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>This app is now running</h1>
+      <div id="container">
+        <FileContainer />
       </div>
     )
   }
