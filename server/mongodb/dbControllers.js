0d907c5a3d0bda8db2966908be0dafe97b6ca6c1
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-10-27 13:37:34 -0400

    changed name files and folder structures

---
+++ server/mongodb/dbControllers.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const dbController = {};
+
+const url =
+  "mongodb://violent-hunters:123abc@ds143143.mlab.com:43143/violent-hunters";
+
+dbController.getDatabase = (req, res, next) => {
+  mongoose.connect(
+    url,
+    { useNewUrlParser: true }
+  );
+  // Runs this logic once there is an open connection with the database
+  mongoose.connection.on("open", () => {
+    // Gets all the collections inside our database and turns it into an array
+    const coll = mongoose.connection.db.listCollections().toArray();
+    // coll returns a promise so we use .then
+    //* We need async await here so that we properly saving the documents in our response
+    coll
+      .then( async collections => {
+        // we loop through the collections and use .find to get all the documents in the collection
+        for (let i = 0; i < collections.length; i++) {
+          // console.log("----Collections----");
+          // console.log(collections[i]);
+          let collectionName = collections[i].name;
+          // * Crashing
+          let Collection = mongoose.model(collectionName, new Schema({}), collectionName);
+          // *Crashing
+
+          // * Await allows us to properly save our documents
+          await Collection.find().then(docs => {
+            res.locals[collectionName] = docs;
+            delete mongoose.connection.models[collectionName];
+          });
+        }
+
+        mongoose.connection.close();
+        next();
+      })
+      .catch(err => console.log("-----CollectionError-----", err));
+  });
+};
+
+module.exports = dbController;
